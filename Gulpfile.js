35d96be8cfef7cd256da5082be39bb54fbf1c653
me <overfl0w@narna.co>
2015-02-12 23:05:45 +0000

    Add a preview page. Demos grid currently. Also adds watch/preview in gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')();
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
 gulp.task('styles', function(){
   return gulp.src('./lib/scss/unicorn.scss')
@@ -14,3 +16,16 @@ gulp.task('scripts', function() {
              .pipe($.tsc())
              .pipe(gulp.dest('./dist/'))
 });
+
+gulp.task('preview', ['styles', 'scripts'], function (){
+  return browserSync({
+    server: {
+      baseDir: ['./preview/', 'dist'],
+    }
+  })
+});
+
+gulp.task('watch', ['preview'], function () {
+  gulp.watch('./lib/scss/*', ['styles', reload])
+  gulp.watch('./lib/ts/*', ['scripts', reload])
+})
